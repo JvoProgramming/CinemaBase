4668bd34eaf23c78fcc17b4147abcca21108db98
JvoProgramming <jvoprogramming@gmail.com>
2021-10-26 23:56:46 -0700

    created spinner component for loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -28,6 +31,21 @@ export default function Home() {
           text={firstResult.overview}
         />
       ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+      <Spinner />
     </>
   );
 }
