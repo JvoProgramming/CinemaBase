6dc8e6ad29dcc26ba0ee7dc78ecf0f9a184e5ebd
JvoProgramming <jvoprogramming@gmail.com>
2021-10-28 19:21:47 -0700

    finished movieinfo component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,8 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 // Components
 import Grid from "./Grid";
 import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
 
 // Hooks
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -19,10 +21,12 @@ const Movie = () => {
 
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie}></MovieInfo>
     </>
   );
 };
